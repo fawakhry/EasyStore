c78204857315e37d743340472745433bff4af62b
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-07-01 15:39:44 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// EasyStore / Matbagy Accounting - CLEAN CONFIG ES32 V1880
+// EasyStore / Matbagy Accounting - CLEAN CONFIG ES33 V1889 STABLE MERGE
 window.TREND_API_URL = "https://script.google.com/macros/s/AKfycbwGHOduL0BHvH-o4up9nbk1wYFi54D2KOnW1AFDigpBzyuAOTWzPfpSFPGSyFVj_fmTmg/exec";
 
-window.EASYSTORE_VERSION = 'ES32 V1880 Clean Core';
-window.EASYSTORE_VERSION_LOCK = 'ES32 V1880 Clean Core';
-window.EASYSTORE_CACHE_TAG = 'es32-v1880-clean-core-20260629';
+window.EASYSTORE_VERSION = 'ES33 V1889 Stable Merge';
+window.EASYSTORE_VERSION_LOCK = 'ES33 V1889 Stable Merge';
+window.EASYSTORE_CACHE_TAG = 'es33-v1889-stable-merge-20260701';
 window.EASYSTORE_AUTO_REFRESH = false; // لا يوجد تحديث كل 10 ثواني
 window.EASYSTORE_INITIAL_LOAD_ONCE = true;
 window.EASYSTORE_DISABLE_PHONE_ACTIVATION = true;
