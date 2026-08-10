a3b1b67a99d59dc6033cb3a05da691dad8473a48
fawakhry <87904105+fawakhry@users.noreply.github.com>
2026-08-10 22:43:18 +0300

    feat: separate laser and print accounting scopes

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-// EasyStore / Matbagy Accounting - ES42 V1917 daily department purchases
+// EasyStore / Matbagy Accounting - ES43 V1918 department accounting scope
 window.TREND_API_URL = "https://script.google.com/macros/s/AKfycbwGHOduL0BHvH-o4up9nbk1wYFi54D2KOnW1AFDigpBzyuAOTWzPfpSFPGSyFVj_fmTmg/exec";
 
-window.EASYSTORE_VERSION = 'ES42 V1917 Daily Department Purchases';
+window.EASYSTORE_VERSION = 'ES43 V1918 Department Accounting Scope';
 window.EASYSTORE_SESSION_CATALOG_FIX = window.EASYSTORE_VERSION;
-window.EASYSTORE_CACHE_TAG = 'es42-v1917-daily-department-purchases-20260810';
+window.EASYSTORE_CACHE_TAG = 'es43-v1918-department-accounting-scope-20260810';
 window.EASYSTORE_AUTO_REFRESH = false; // لا يوجد تحديث كل 10 ثواني
 window.EASYSTORE_INITIAL_LOAD_ONCE = true;
 window.EASYSTORE_DISABLE_PHONE_ACTIVATION = true;
@@ -16,3 +16,4 @@ window.EASYSTORE_V1914_LEGACY_DEBT_RECONCILE = true;
 window.EASYSTORE_V1915_CUSTOMER_ACCOUNTS = true;
 window.EASYSTORE_V1916_CUSTOMER_ACCOUNT_DRAWER = true;
 window.EASYSTORE_V1917_DAILY_DEPARTMENT_PURCHASES = true;
+window.EASYSTORE_V1918_DEPARTMENT_ACCOUNTING_SCOPE = true;
